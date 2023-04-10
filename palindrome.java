80cdb150125aecb8abeccec4803f4fbf965fdff1
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-04-10 14:49:55 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+/* Online Java Compiler and Editor */
+import java.util.*;
+public class palindrome{
+
+     public static void main(String []args){
+         String a = "abcd";
+         String b = "dabc";
+         int count = 0;
+   HashSet<String> set = new HashSet<>();
+   for(int i=0;i<a.length();i++){
+      set.add(Character.toString(a.charAt(i)));
+      
+       // System.out.println(a.charAt(i));
+   }
+   for(int i=0;i<b.length();i++){
+     if(set.contains(Character.toString(b.charAt(i)))){
+         count++;
+        
+     }
+      
+       // 
+   }  
+   if(count == a.length()){
+      System.out.println("yes");  
+   }else{
+        System.out.println("no");
+   }
+      
+     }
+}
