932b0f124ee7571d2cc3cfc43645be0ed8e511cf
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-04-11 14:02:32 +0530

    power of two

---
+++ powerOfTwo.java
@@ -0,0 +1,26 @@
+/* Online Java Compiler and Editor */
+import java.util.*;
+public class powerOfTwo{
+
+     public static void main(String []args){
+   
+  
+     System.out.println(power(6));
+   
+             
+
+     }
+
+     public static boolean power(int n){
+        if(n==0){
+            return false;
+        }
+        while(n!=1){
+            if(n%2!=0){
+                return false;
+            }
+            n = n/2;
+        }
+        return true;
+     }
+}
